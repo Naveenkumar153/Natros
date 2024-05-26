32f8edcf7588598bb125ac85ad8fcff93d3af30f
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-05-26 12:01:35 +0530

    feat: implemented clean code and modules and controllers

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const tourRouter = require('./src/routes/tours/tours.router');
+const userRouter = require('./src/routes/users/users.router');
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}else if(process.env.NODE_ENV === 'production'){
+    app.use(morgan('tiny'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+
+
+module.exports = app;
+
