43d3734470a28f917e33b0981e631cfe2a0a9eb1
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-06-16 20:57:54 +0530

    fix: remove unwanted codes

--- src/routes/tours/tours.router.js
+++ src/routes/tours/tours.router.js
@@ -8,11 +8,11 @@ const tourValidation = require('../../controllers/tours/tours.validation');
 + * Check if the parameter 'id' exists in the request parameters
 + * @param {id} id - The id of the tour
 */
-tourRouter.param('id', tourController.checkParamId);
+// tourRouter.param('id', tourController.checkParamId);
 
 tourRouter.route('/')
    .get(tourController.getTours)
-   .post(tourValidation.checkBody,tourController.createTour)
+   .post(tourController.createTour)
 
 tourRouter.route('/:id')
    .get(tourController.getTourById)
