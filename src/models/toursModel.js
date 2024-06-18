b9b30256518c8fbeda13dbe835ae99484ef615f5
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-06-18 22:25:51 +0530

    feat: implemented more schema

--- src/models/toursModel.js
+++ src/models/toursModel.js
@@ -3,7 +3,18 @@ const mongoose = require('mongoose');
 const tours = new mongoose.Schema({
     name:{ type:String, required:true, unique:true },
     price:{ type:Number, required:true },
-    rating: { type: Number, required:true },
+    ratingsAverage: { type: Number, required:true },
+    duration: { type: Number, required:true },
+    maxGroupSize: { type: Number, required:true },
+    difficulty: { type: String, required:true },
+    ratingsQuantity: { type:Number, default:0 },
+    priceDiscount:Number,
+    summary:{ type:String, trim:true, required:true },
+    description:{ type:String, trim:true, },
+    imageCover:{ type:String, required:true },
+    images:[String],
+    createdAt:{ type:Date, default:Date.now() },
+    startDates: [Date]
 });
 
 module.exports =  mongoose.model('Tours', tours);
