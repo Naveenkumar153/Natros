cadd3fcfdfaf73806cc7f32ae7b2159aef6041e6
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-07-05 07:39:09 +0530

    feat: implemented the api filtering, advanced filtering, sorting, rate limiting

--- src/controllers/tours/tours.controllers.js
+++ src/controllers/tours/tours.controllers.js
@@ -7,16 +7,59 @@ exports.checkParamId = (req,res,next) => {
 };
 
 exports.getTours = async (req, res) => {
-
   try {
-    const tours = await toursModel.find();
+
+    // BUILD QUERY
+    let queryObj = { ...req.query };
+    const excludedFields = ['page','sort','limit','fields'];
+    excludedFields.forEach(field => delete queryObj[field]);
+    
+    // ADVANCED FILTERING
+    
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    let query = toursModel.find(JSON.parse(queryStr));
+
+    // SORTING
+    if(req.query.sort){
+      query = query.sort(req.query.sort)
+    }else{
+      query = query.sort('-createdAt');
+    }
+
+    
+    // RATE LIMITING
+
+    if(req.query.fields){
+      const fields = req.query.fields.split(',').join(' ');
+      console.log('fields',fields);
+      query = query.select(fields);
+    }else{
+      query = query.select('-__v');
+    };
+
+    
+    // Execute query
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
+      result:tours.length,
       data: {
         tours
       }
     });
+
+    // const tours = await toursModel.find({
+    //   duration: req.query.duration,
+    //   difficulty: req.query.difficulty
+    // });
+
+    // const tours = await toursModel.find()
+    // .where('duration').equals(req.query.duration)
+    // .where('difficulty').equals(req.query.difficulty)
+
+    
   } catch (error) {
     res.status(500).json({
       status: 'error',
