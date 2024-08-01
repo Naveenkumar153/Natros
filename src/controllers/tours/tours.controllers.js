a6a0b7de0d9c5ab94e826f5dfc3f66b2d8bfb157
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-08-01 07:54:12 +0530

    refactor: implemented the clean & reuseable code

--- src/controllers/tours/tours.controllers.js
+++ src/controllers/tours/tours.controllers.js
@@ -1,5 +1,6 @@
 
 let toursModel  = require('../../models/toursModel');
+let ApiFeatures = require('../../utils/api-features.class');
 
 
 exports.checkParamId = (req,res,next) => {
@@ -9,51 +10,14 @@ exports.checkParamId = (req,res,next) => {
 exports.getTours = async (req, res) => {
   try {
 
-    // BUILD QUERY
-    let queryObj = { ...req.query };
-    const excludedFields = ['page','sort','limit','fields'];
-    excludedFields.forEach(field => delete queryObj[field]);
-    
-    // ADVANCED FILTERING
-    
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    let query = toursModel.find(JSON.parse(queryStr));
+    const features = new ApiFeatures(toursModel.find(), req.query)
+    .filter()
+    .sort()
+    .rateLimit()
+    .pagination();
 
-    // SORTING
-    if(req.query.sort){
-      query = query.sort(req.query.sort)
-    }else{
-      query = query.sort('-createdAt');
-    }
-
-    
-    // RATE LIMITING
-
-    if(req.query.fields){
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    }else{
-      query = query.select('-__v');
-    };
-
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if(req.query.page){
-      const numToursQuery = toursModel.countDocuments(JSON.parse(queryStr));
-      const numTours = await numToursQuery;
-      if(skip >= numTours){
-        throw new Error('This page does not exist!');
-      }
-    };
-    
-    // Execute query
-    const tours = await query;
+    const tours = await features.query;
+    console.log('features',tours);
 
     res.status(200).json({
       status: 'success',
@@ -62,6 +26,7 @@ exports.getTours = async (req, res) => {
         tours
       }
     });
+    
   } catch (error) {
     res.status(500).json({
       status: 'error',
---
+++ src/utils/api-features.class.js
@@ -0,0 +1,60 @@
+class ApiFeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    };
+
+    filter(){
+        let queryObj = { ...this.queryString };
+        const excludedFields = ['page','sort','limit','fields'];
+        excludedFields.forEach(field => delete queryObj[field]);
+
+        // ADVANCED FILTERING
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        this.query  = this.query.find(JSON.parse(queryStr));
+        return this;
+    };
+
+    sort(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+          }else{
+            this.query = this.query.sort('-createdAt');
+          }
+
+          return this;
+    };
+
+    pagination(){
+        const page  = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 100;
+        const skip  = (page - 1) * limit;
+        this.query  = this.query.skip(skip).limit(limit);
+
+        // if(this.queryString.page){
+        // const numToursQuery = await toursModel.countDocuments(JSON.parse(queryStr));
+        // const numTours = await numToursQuery;
+        //     if(skip >= numTours){
+        //         throw new Error('This page does not exist!');
+        //     }
+        // };
+        
+        return this;
+    };
+
+    rateLimit(){
+        if(this.queryString.fields){
+        const fields = this.queryString.fields.split(',').join(' ');
+            this.query = this.query.select(fields);
+        }else{
+            this.query = this.query.select('-__v');
+        };
+
+        return this;
+    }
+};
+
+module.exports = ApiFeatures;
