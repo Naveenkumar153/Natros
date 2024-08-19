aacb840bbb259d9298bb054e279c432bd115689f
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-08-19 08:44:18 +0530

    feat: handle the more mongose validation key duplicate and invalid ids

--- src/controllers/error/error.controllers.js
+++ src/controllers/error/error.controllers.js
@@ -1,5 +1,42 @@
+
+const AppError = require('../../utils/appError');
+
+const handelCaseErrorDB = error => {
+    const message = `Invalid ${error.path}: ${error.message}`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDb = err => {
+    const message = `Duplicate filed value '${err.keyValue.name}'. Please use another value!`;
+    return new AppError(message, 400);
+};
+
+
+const handleProductionError = (err,res) => {
+    if(err.isOperational){
+
+        // Operational, trusted error: sent message to client
+       res.status(err.statusCode).json({
+           status: err.status,
+           message: err.message || 'Something went wrong. Please try again!',
+       });
+
+   }else{
+        // Programming or other unknow error: don't want leak error to the client
+        
+           // 1). Login the error
+        //    console.error('ERROR ', err);
+
+           // 2). Some generic message
+           res.status(err.statusCode || 500).json({
+               status: err.status || 'error',
+               message: err.message || 'Something went wrong. Please try again!',
+           });
+   }
+}
+
 exports.globalErrorHandler = (err, req, res, next) => {
-    
+    console.log('err',err);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
    
@@ -13,28 +50,11 @@ exports.globalErrorHandler = (err, req, res, next) => {
         });
 
     } else if (process.env.NODE_ENV === "production") {
+        if(err.name === 'CastError') err = handelCaseErrorDB(err); 
 
-        if(err.isOperational){
+        if(err.code === 11000) err = handleDuplicateFieldDb(err);
 
-             // Operational, trusted error: sent message to client
-            res.status(err.statusCode).json({
-                status: err.status,
-                message: err.message || 'Something went wrong. Please try again!',
-            });
-
-        }else{
-             // Programming or other unknow error: don't want leak error to the client
-             
-                // 1). Login the error
-                console.error('ERROR ', err);
-
-                // 2). Some generic message
-                res.status(500).json({
-                    status: 'error',
-                    message: 'Something went wrong. Please try again!',
-                });
-        }
-
+        handleProductionError(err,res);
     }
 
 };
