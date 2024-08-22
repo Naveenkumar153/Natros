191660b7cc53892ea3c72233ba286b584dc8e645
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-08-22 07:44:09 +0530

    fix: handle the db level validation

--- src/controllers/error/error.controllers.js
+++ src/controllers/error/error.controllers.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldDb = err => {
     return new AppError(message, 400);
 };
 
+const handleValidationErrorDb = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+
 
 const handleProductionError = (err,res) => {
     if(err.isOperational){
@@ -25,7 +31,7 @@ const handleProductionError = (err,res) => {
         // Programming or other unknow error: don't want leak error to the client
         
            // 1). Login the error
-        //    console.error('ERROR ', err);
+           console.error('ERROR ', err);
 
            // 2). Some generic message
            res.status(err.statusCode || 500).json({
@@ -36,7 +42,6 @@ const handleProductionError = (err,res) => {
 }
 
 exports.globalErrorHandler = (err, req, res, next) => {
-    console.log('err',err);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
    
@@ -50,9 +55,12 @@ exports.globalErrorHandler = (err, req, res, next) => {
         });
 
     } else if (process.env.NODE_ENV === "production") {
-        if(err.name === 'CastError') err = handelCaseErrorDB(err); 
-
-        if(err.code === 11000) err = handleDuplicateFieldDb(err);
+        
+        if(err.name === 'CastError'){
+            err = handelCaseErrorDB(err); 
+        }else if(err.code === 11000){
+            err = handleDuplicateFieldDb(err);
+        }
 
         handleProductionError(err,res);
     }
