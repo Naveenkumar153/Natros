f2f9172dfc26d9657429c52826830bbcd69f4f9b
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-08-25 15:00:50 +0530

    feat: get all users information from db

--- src/controllers/users/users.controller.js
+++ src/controllers/users/users.controller.js
@@ -1,9 +1,18 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        stauts:'error',
-        message: ' This route is not yet defined! ',
+const user = require('../../models/userModel');
+const { catchAsync } = require('../../utils//catchAsync');
+
+
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+
+    const users = await user.find();
+
+    res.status(200).json({
+        stauts:'sucess',
+        results:users.length,
+        data:{ users }
     });
-  };
+  });
 exports.getUser = (req, res) => {
     res.status(500).json({
         stauts:'error',
