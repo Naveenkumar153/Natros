994bff932c8a2e0edf144d319b599e380947ded5
Naveenkumar153 <naveenkumartech2025@gmail.com>
2024-08-27 08:22:53 +0530

    feat: changedPasswordAfter verification implemented

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -28,6 +28,7 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords do not match'
         }
     },
+    passwordChangedAt: Date,
 });
 
 userSchema.pre('save',  (async function(next) {
@@ -44,7 +45,18 @@ userSchema.pre('save',  (async function(next) {
 
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
-}
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if(this.passwordChangedAt) {
+        console.log('this.passwordChangedAt',this.passwordChangedAt);
+        console.log('JWTTimestamp',JWTTimestamp);
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        return JWTTimestamp < changedTimestamp;
+    }
+    // false means not changed
+    return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
